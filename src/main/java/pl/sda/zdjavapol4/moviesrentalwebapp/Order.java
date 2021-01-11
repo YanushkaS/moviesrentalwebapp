1c381a4a47d5d385e51f9b0b8e944e1aff6b4a0b
Yana Sobchuk <jana.sobczuk@gmail.com>
2021-01-11 22:22:53 +0100

    Init

---
+++ src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/Order.java
@@ -0,0 +1,11 @@
+package pl.sda.zdjavapol4.moviesrentalwebapp;
+
+public class Order {
+
+    private int copyId;
+    private int movieID;
+    private String courier;
+    private String orderStatus;
+
+
+}
--- src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/Review.java
+++ src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/Review.java
@@ -1,4 +1,7 @@
 package pl.sda.zdjavapol4.moviesrentalwebapp;
 
 public class Review {
+    private int revewId;
+    private int rate;
+    private int clientId;
 }
